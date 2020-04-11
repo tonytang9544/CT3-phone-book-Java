fc9345c8fa7283471ecf92d09f69caadde54c49f
Tony Tang <tonytang9544@hotmail.com>
2020-04-11 01:33:56 +0100

    Added conversion from Person to subclasses. Added corresponding tests.

---
+++ src/ct3PhoneBook/contactObjects/tests/FriendTest.java
@@ -0,0 +1,12 @@
+package ct3PhoneBook.contactObjects.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FriendTest {
+
+    @Test
+    void convertPersonToFriend() {
+    }
+}
