335a27d6f5645074086b00d6944587eebb48da3a
Tony Tang <tonytang9544@hotmail.com>
2020-04-20 01:02:10 +0100

    Added a few JavaDoc. Added delEntry() to ContactList

--- src/ct3PhoneBook/contactList/ContactList.java
+++ src/ct3PhoneBook/contactList/ContactList.java
@@ -7,14 +7,35 @@ import java.util.Collections;
 import java.util.List;
 
 public class ContactList {
+
     private final List<Person> contactList;
 
+    /**
+     * Constructor creates an empty list of People
+     */
     public ContactList() {
         contactList = new ArrayList<Person>();
     }
 
-    public void addEntry(Person person) {
-        contactList.add(person);
+    public void addEntry(Person personToAdd) {
+        contactList.add(personToAdd);
+    }
+
+    /**
+     * Delete the Person that matches the person input, using List.remove()
+     * Return true if deleted, return false if not found or NullPointerException
+     *
+     * @param personToDelete the Person object that potentially matches a member
+     *                       in the ContactList
+     * @return  true if successfully deleted a Person, false if not found.
+     */
+    public boolean delEntry(Person personToDelete) {
+        try {
+            return contactList.remove(personToDelete);
+        }
+        catch (NullPointerException e){
+            return false;
+        }
     }
 
     public Person getEntryByIndex(int index) {
@@ -29,6 +50,13 @@ public class ContactList {
         Collections.sort(this.contactList, new SortByPhoneNumber());
     }
 
+    /**
+     * Find Person based on any description of that person
+     * Return a ContactList of Person if found
+     * Return an empty ContactList if not found
+     * @param keyWord the String for query
+     * @return ContactList with zero or more People inside.
+     */
     public ContactList findEntryByString(String keyWord) {
         ContactList matchedPerson = new ContactList();
         for (Person person : this.contactList) {
--- src/ct3PhoneBookTest/contactList/ContactListTest.java
+++ src/ct3PhoneBookTest/contactList/ContactListTest.java
@@ -187,6 +187,7 @@ class ContactListTest {
         contactList.addEntry(friend2);
         contactList.addEntry(workFriend);
         // Finished initialization, start testing
+        // A positive test
         ContactList personsFound = contactList.findEntryByString("Tony");
         assertEquals(3, personsFound.getNumberOfEntries());
         assertEquals("077",
@@ -195,5 +196,60 @@ class ContactListTest {
                 ((Friend)personsFound.getEntryByIndex(1)).getShortNotes());
         assertEquals("Mark",
                 personsFound.getEntryByIndex(2).getName());
+        // A negative test
+        ContactList noOneShouldBeFound = contactList.findEntryByString("Steve");
+        assertEquals(0, noOneShouldBeFound.getNumberOfEntries());
+    }
+
+    @Test
+    public void delEntry() {
+        Person person = new Person("Tony", "077");
+        Friend friend1 = Friend.convertPersonToFriend(
+                person,
+                new GregorianCalendar(1995, 1,1),
+                "some nobody.");
+        friend1.setPhoneNumber("07777");
+        Friend friend2 = new Friend(
+                "Paul",
+                "088",
+                new GregorianCalendar(1995, 1,1),
+                "A nice guy");
+        WorkFriend workFriend = new WorkFriend(
+                "Mark",
+                "0777777",
+                "Tony's",
+                CompanyPosition.CEO);
+        ContactList contactList = new ContactList();
+        contactList.addEntry(person);
+        contactList.addEntry(friend1);
+        contactList.addEntry(friend2);
+        contactList.addEntry(workFriend);
+        // Finished initialization, start testing
+        // A trivial positive test
+        assertTrue(contactList.delEntry(person));
+        assertEquals(3, contactList.getNumberOfEntries());
+        assertEquals("07777",
+                contactList.getEntryByIndex(0).getPhoneNumber());
+        assertEquals("A nice guy",
+                ((Friend)contactList.getEntryByIndex(1)).getShortNotes());
+        assertEquals("Mark",
+                contactList.getEntryByIndex(2).getName());
+        // A trivial negative test, created an identical Person,
+        // but ID is different so should not match existing ContactList
+        Friend friend3 = new Friend(
+                "Paul",
+                "088",
+                new GregorianCalendar(1995, 1,1),
+                "A nice guy");
+        assertFalse(contactList.delEntry(friend3));
+        assertEquals(3, contactList.getNumberOfEntries());
+        // Another trivial negative test, null Person pointer
+        assertFalse(contactList.delEntry(null));
+        // A non-trivial positive test,
+        // try to delete someone found in the ContactList
+        ContactList foundPerson = contactList.findEntryByString("Mark");
+        assertEquals(1, foundPerson.getNumberOfEntries());
+        assertTrue(contactList.delEntry(foundPerson.getEntryByIndex(0)));
+        assertEquals(2, contactList.getNumberOfEntries());
     }
 }
